c8d801e27ad3493dea55bd5f5b7a9ea9a41855a8
yang yang <wenzhixuanyy@163.com>
2026-03-29 14:55:52 +0800

    Initial Commit

---
+++ eatheal/eathealApp.swift
@@ -0,0 +1,17 @@
+//
+//  eathealApp.swift
+//  eatheal
+//
+//  Created by Yang Yang on 2026/3/29.
+//
+
+import SwiftUI
+
+@main
+struct eathealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
